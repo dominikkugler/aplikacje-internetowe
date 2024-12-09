fdf0a896b4a601a0c4dea6832eefc430e22dd279
Dominik <20456@student.ans-elblag.pl>
2024-12-09 10:29:17 +0100

    switching to globals css (dont wanna talk about it)

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 "use client";
 import Link from 'next/link';
+import '../app/globals.css';
 
 export default function Footer() {
   return (
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 "use client";
+import '../app/globals.css';
+
 export default function Header() {
   return (
     <>
