b57f4e83c5fac9047768361b9b5fd5720fe33205
Dominik <20456@student.ans-elblag.pl>
2024-12-09 09:34:30 +0100

    quick fix 5

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><Link href="/about">O mnie</Link></li>
+        <li><Link href="/">O mnie</Link></li>
         <li><Link href="/interests">Zainteresowania</Link></li>
         <li><Link href="/favorite">Ulubiony film</Link></li>
         <li><Link href="/contact">Kontakt</Link></li>
--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ const nextConfig = {
     basePath: '/aplikacje-internetowe',
     assetPrefix: './',
     trailingSlash: true,
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/about', // Redirect root to /about
-          permanent: true, // Permanent (301) redirect
-        },
-      ];
-    },
   };
   
   export default nextConfig;
