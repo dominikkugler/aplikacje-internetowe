000013c6b406617b67f844c0e361460fd413727f
dominikkugler <119433331+dominikkugler@users.noreply.github.com>
2024-12-10 11:25:26 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/aplikacje-internetowe',
     assetPrefix: './',
     trailingSlash: true,
     async redirects() {
